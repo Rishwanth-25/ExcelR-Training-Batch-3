9d2c6dc3c13404861054bf910bb98b9a958e36a1
Rishwanth-25 <rishibadinapalli@gmail.com>
2025-01-03 21:30:29 +0530

    User Credentials2 using while loop

---
+++ Demo006.java
@@ -0,0 +1,11 @@
+public class Demo006 {
+    public static void main(String[] args) {
+        String Username = "Rish019";
+        String Pass = "Rishi_019";
+        while((Username == "Rishi019") && (Pass == "Rishi_019")) {
+            System.out.println("Welcome to github");
+            break;
+            
+        }
+    }
+}
