aa569e120efff27972e7ec75a93fcccfce65415b
Rishwanth-25 <rishibadinapalli@gmail.com>
2025-01-06 20:26:50 +0530

    Displaying details of customer2

---
+++ Demo010.java
@@ -0,0 +1,29 @@
+import java.util.*;
+ class Customer {
+    
+        String Cust_name = "rishi";
+        int Account_no = 1234567890;
+        Double Balance = 100.00;
+        void accept_details(){
+
+            Scanner sc = new Scanner(System.in);
+            Cust_name = sc.nextLine();
+            Account_no =sc.nextInt();
+        }
+        void Display_details(){
+        System.out.println("Hello "+Cust_name);
+        System.out.println("Ur Balance is "+Balance);
+        }
+        }
+    public class Demo010{
+        public static void main(String[] args){
+            Demo010 obj = new Demo010();
+            obj
+
+        }
+    }
+
+
+
+        
+
