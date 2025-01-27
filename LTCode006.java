28e039c42a2347040c66487c3022e95f1c6581bc
Rishwanth-25 <rishibadinapalli@gmail.com>
2025-01-27 12:55:52 +0530

    Remove Duplicates from Sorted Array

---
+++ LTCode006.java
@@ -0,0 +1,26 @@
+class LTCode006 {
+    public int removeDuplicates(int[] nums) {
+        int k = 0;
+        for(int x : nums){
+            if(k == 0 || x != nums[k-1]){
+                nums[k++] = x;
+            }
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        LTCode006 solution = new LTCode006();
+        
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4}; 
+        int k = solution.removeDuplicates(nums);
+        
+        System.out.println("Number of unique elements: " + k);
+        System.out.print("Array with unique elements: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
+
+
