5025a71a00915f2b6931a56e65b7c7443a0497c7
Rishwanth-25 <rishibadinapalli@gmail.com>
2025-01-27 12:52:26 +0530

    check number is palindrome or not

---
+++ Demo051.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Demo051{
+
+    public static boolean isPalindrome(int number) {
+        int originalNumber = number;
+        int reversedNumber = 0;
+        int remainder;
+
+        while (number != 0) {
+            remainder = number % 10;
+            reversedNumber = reversedNumber * 10 + remainder;
+            number = number / 10; 
+        }
+
+        return originalNumber == reversedNumber;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);       
+    
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome.");
+        } else {
+            System.out.println(number + " is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
