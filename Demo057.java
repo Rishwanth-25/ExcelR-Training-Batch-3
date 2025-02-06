b8eee00b7d2acc86bc3e5bc677cf4e0b99c2331c
Rishwanth-25 <rishibadinapalli@gmail.com>
2025-02-06 12:50:43 +0530

    print sum of first and last digits of a number

---
+++ Demo057.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Demo057 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int sum = sumFirstAndLastDigit(number);
+
+        System.out.println("Sum of first and last digits of " + number + " is: " + sum);
+
+        scanner.close();
+    }
+    public static int sumFirstAndLastDigit(int number) {
+        int lastDigit = number % 10;
+        while (number >= 10) {
+            number /= 10;
+        }
+        int firstDigit = number;
+
+        return firstDigit + lastDigit;
+    }
+}
