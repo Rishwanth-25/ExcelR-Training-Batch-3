850b78fe0cd7c46d14fa831cb36c55d970b8cd9c
Rishwanth-25 <rishibadinapalli@gmail.com>
2025-02-06 12:58:44 +0530

    print count even numbers of an array

---
+++ Demo068.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Demo068 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (array[i] % 2 == 0) {
+                count++;
+            }
+        }
+
+        System.out.println("Number of even numbers in the array: " + count);
+
+        scanner.close();
+    }
+}
